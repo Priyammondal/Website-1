647d6ae94bf520978a949ca5309ed5158fffd7c2
Priyam Mondal <priyammondal096@.com>
2021-04-13 01:35:02 +0530

    responsive-1

--- website-1/src/Header/Header.js
+++ website-1/src/Header/Header.js
@@ -2,17 +2,19 @@ import './Header.css';
 function Header() {
     return (
         <nav>
-            <span id="text">
-                Please input your information,for a personalized quote
-            </span>
-            <span id="input">
-                <input type="text"></input>
-                <p>Your Progress</p>
-            </span>
-            <span id="ApplicationNo">
-                <p style={{color: "red"}}>Your Application ID</p>
-                <p>ORC 4893020230</p>
-            </span>
+            <div id="text">
+                Please input your information, for a personalized quote
+            </div>
+            <div className="new">
+                <span id="input">
+                    <input type="text"></input>
+                    <div className="progress">Your Progress</div>
+                </span>
+                <span id="ApplicationNo">
+                    <p style={{ color: "tomato" }}>Your Application ID</p>
+                    <p>ORC 4893020230</p>
+                </span>
+            </div>
         </nav>
     )
 }
